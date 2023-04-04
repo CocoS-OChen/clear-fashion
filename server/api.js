9c3a4ceb7202705b0d5f5fda7dfa355b0e1027ed
CocoS-OChen <cocogrep@gmail.com>
2023-04-04 20:16:14 +0200

    feat(get-product): get a specific product

--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const MongoClient = require('./mongoDb');
+
 
 const PORT = 8092;
 
@@ -14,8 +16,80 @@ app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+app.get('', async (req, res) => {
+  console.log("/produits/recherche, input : ", req.query);
+  
+  const body = {
+    success: true,
+    data: {}
+  };
+
+  let brandName = req.query.marque || null;
+  let lessThanPrice = req.query.prix ? parseFloat(req.query.prix) : null;
+  let limitCount = req.query.limite ? parseInt(req.query.limite) : null;
+
+  const produits = await MongoClient.fetchProduits(brandName, lessThanPrice);
+  let result = limitCount !== null ? produits.slice(0, limitCount) : produits;
+
+  body.data.resultat = result;
+
+  res.send(body);
+});
+
+
+
+app.get('/products/search', async (req, res) => {
+  console.log("/products/search, input : ", req.query);
+
+  const responseBody = {
+    success: true,
+    data: {}
+  };
+
+  let brandName = req.query.brand || null;
+  let lessThanPrice = req.query.price ? parseFloat(req.query.price) : null;
+  let limitCount = req.query.limit ? parseInt(req.query.limit) : null;
+
+  const produits = await MongoClient.fetchProducts(brandName, lessThanPrice);
+  let result = limitCount !== null ? produits.slice(0, limitCount) : produits;
+
+  responseBody.data.result = result;
+
+  res.send(responseBody);
+});
+
+
+app.get('/products/*', async (req, res) => {
+  console.log("products/:id, input : ", req.params[0]);
+
+  const responseBody = {
+    success: true,
+    data: {}
+  };
+
+  const uuid = req.params[0];
+  const product = await MongoClient.fetchProductByUuid(uuid);
+
+  responseBody.data.resultat = product;
+
+  res.send(responseBody);
+});
+
+
+
+app.get('/brands', async (req, res) => {
+  console.log("/brands, input : ", req.query);
+
+  const responseBody = {
+    success: true,
+    data: {}
+  };
+
+  const brands = await MongoClient.fetchBrands();
+
+  responseBody.data.brands = brands;
+
+  res.send(responseBody);
 });
 
 app.listen(PORT);
